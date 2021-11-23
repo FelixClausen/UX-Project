66fabbc33736916f24e28d58df45d9beb5a000f8
Felix Clausen <Felix-Clausen@outlook.com>
2021-11-23 15:51:57 +0100

    added github commands in JS file

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("testfile nr 2")
+
+
+// switching branches
+// git switch BranchName
+
+
+// pushing code to github
+// git add .
+// git commit -m "comment here"
+// git push
+
+
+// updating merged code from github
+// git fetch
+// git rebase origin/Master
