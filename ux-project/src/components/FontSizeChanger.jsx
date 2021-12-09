a44c47349cf1f63bcff78cab8e17e35cc439afad
Mateusz-1 <58937337+Mateusz-1@users.noreply.github.com>
2021-12-09 20:49:57 +0100

    Add files via upload
    Font size changer, tanken är att den ska anpassas efter classNames och appliceras på text i webbapplikationen.

---
+++ ux-project/src/components/FontSizeChanger.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function FontSizeChanger() {
+  const [fontSize, setFontSize] = useState(16);
+  
+  return (
+    <div className='btnWrapper'>
+      <div className='fontSize'>
+        <button className='resizeUp' onClick={() => setFontSize(fontSize + 2)}> 
+            + Öka storlek
+          </button>
+          <button className='resizeDown' onClick={() => setFontSize(fontSize - 2)}>
+            - Minska storlek
+           </button> 
+        <p 
+          style={{
+            fontSize: `${fontSize}px`
+        }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FontSizeChanger;
+
