d76f8e301fb1ce95b4a6e3ec6426bb6b9dc425d7
Mateusz-1 <58937337+Mateusz-1@users.noreply.github.com>
2022-01-04 19:17:07 +0100

    mindre textändring

--- ux-project/src/WorkShop.jsx
+++ ux-project/src/WorkShop.jsx
@@ -12,8 +12,8 @@ const WorkShop = () => {
   return (
     <div>
       <div className="workshopIntro">
-        <h1>WorkShop's</h1>
-        <h3>WorkShop är en sammanställning av likasinnade som vill lära sig mer och gå in på djupet om klimat frågan.</h3>
+        <h1>Workshops</h1>
+        <h3>Workshop är en sammanställning av likasinnade som vill lära sig mer och gå in på djupet om klimat frågan.</h3>
         <p>För att delta behöver du registerade dig nedan. Det är helt gratis. Det enda du behöver är tillgång till en surfplatta eller dator. Du behöver heller inga förkunskaper om ämnet.</p>
         <p className="welcome">Välkommen!</p>
       </div>
