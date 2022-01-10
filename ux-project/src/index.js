810f3a46e7ed99c8f3f2a58d15358827b4b86410
Felix Clausen <Felix-Clausen@outlook.com>
2022-01-10 11:01:47 +0100

    tog bort en kommentar

--- ux-project/src/index.js
+++ ux-project/src/index.js
@@ -17,22 +17,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-/*
-kommandon för git
-
-byta branches
- git switch BranchName
-
- kolla vilken branch du är i
-  git branch
-
-lägga upp på github
- git add .
- git commit -m "comment here"
- git push
-
-uppdatera från github
- git fetch
- git rebase origin/Master
-*/
